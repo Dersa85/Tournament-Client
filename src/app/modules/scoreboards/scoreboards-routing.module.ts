722851a3c678aa3678c5116943a72284802936c1
Dersa <dersa85@googlemail.com>
2021-12-30 15:23:18 +0100

    refactoring routing

--- src/app/modules/scoreboards/scoreboards-routing.module.ts
+++ src/app/modules/scoreboards/scoreboards-routing.module.ts
@@ -5,18 +5,16 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    component: ScoreboardsComponent,
-    children: [
-      {
-        path: 'best-of-3',
-        loadChildren: () => import('./best-of-3/best-of-3.module').then(m => m.BestOf3Module),
-      },
-      {
-        path: 'team-point',
-        loadChildren: () => import('./team-point/team-point.module').then(m => m.TeamPointModule),
-      }
-    ]
+    component: ScoreboardsComponent
   },
+  {
+    path: 'best-of-3',
+    loadChildren: () => import('./best-of-3/best-of-3.module').then(m => m.BestOf3Module),
+  },
+  {
+    path: 'team-point',
+    loadChildren: () => import('./team-point/team-point.module').then(m => m.TeamPointModule),
+  }
   
 ];
 
