c30362a08c8983231052601d4206feb6967751ca
Dersa <dersa85@googlemail.com>
2022-01-24 15:05:02 +0100

    add: save data in file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   socketConfig : {
-    url: 'http://raspberrypi:7878',
-    options: {}
+    url: 'http://localhost:7878',
+    options: { transports : ['websocket'] }
   }
 };
